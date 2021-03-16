2955c4b0b13eb3bf66aff1934d1bde58c1ff025a
priyadharshini18-hub <62538322+priyadharshini18-hub@users.noreply.github.com>
2021-03-16 03:24:25 -0400

    Add files via upload

---
+++ Phantom_Hangman.java
@@ -0,0 +1,12 @@
+package phantom_hangman;
+
+public class Phantom_Hangman {
+
+    public static void main(String[] args)
+    {
+        phantom_hangman.AudioPlayerExample1 a=new phantom_hangman.AudioPlayerExample1();
+        phantom_hangman.GameBoard newPhantomHangman = new phantom_hangman.GameBoard(a);
+
+    }
+
+}
